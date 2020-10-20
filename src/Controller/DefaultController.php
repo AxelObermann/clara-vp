c546f20c7f141e0906805c984acf769692f83548
Spongebob <obermann@msw-werbeagentur.de>
2020-10-20 17:13:07 +0200

    sdf

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +14,33 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController1',
+        ]);
+    }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboard()
+    {
+        return $this->render('default/dashboard.html.twig',[
+            'title' => 'Dashboard'
+        ]);
+    }
+
+    /**
+     * @Route("/createAdmin")
+     */
+    public function createAdminUser(EntityManagerInterface $entityManager){
+        $user = new User();
+        $user->setDeleted(false);
+        $user->setActive(false);
+        $user->setDisplayName('Axel Obermann');
+        $user->setEmail('info@bestofstickers.de');
+
+        $entityManager->persist($user);
+        $entityManager->flush();
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController1',
         ]);
