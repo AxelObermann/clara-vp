66ded53f9cde5978a88b8c1775032d908696d6f5
Spongebob <obermann@msw-werbeagentur.de>
2020-11-03 11:03:25 +0100

    abfrage ob alter Benutzer

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -120,37 +120,40 @@ class ImportController extends AbstractController
         $users = $userRepository->findAll();
         $conn = \Doctrine\DBAL\DriverManager::getConnection($this->connectionParams);
         foreach ($users as $user ){
-            $sql = "SELECT * FROM calendars where user=".$user->getOldid();
-            $stmt = $conn->query($sql); // Simple, but has several drawbacks
-            $stmt->execute();
-            $row = $stmt->fetch();
-            if ($stmt->rowCount() != 0){
-                while ($row = $stmt->fetch()){
-                    $cal = new Calendar();
-                    $cal->setTitle($row['title']);
-                    $cal->setDetails($row['details']);
-                    $cal->setStart(new DateTime($row['start']));
-                    $cal->setEnd(new DateTime($row['end']));
-                    $cal->getAllday($row['all_day']);
-                    $cal->setStatus($row['status']);
-                    $cal->setActive($row['active']);
-                    $cal->setUser($user);
-                    $cal->setColor($row['color']);
-                    $cal->setCustomerName($row['Kundenname']);
-                    $cal->setStreet($row['Straße']);
-                    $cal->setStreetNumber($row['HausNr']);
-                    $cal->setPlz($row['PLZ']);
-                    $cal->setOrt($row['Ort']);
-                    $cal->setAnsprechpartner($row['Ansprechpartner']);
-                    $cal->setAnrede($row['anrede']);
-                    $cal->setEmail($row['Emailadresse']);
-                    $cal->setBermerkungen($row['Bemerkungen']);
-                    $cal->setTerminType($row['calTerminTppe']);
-                    $entityManager->persist($cal);
-                    $entityManager->flush();
-                    $count++;
+            if ($user->getOldid() != ""){
+                $sql = "SELECT * FROM calendars where user=".$user->getOldid();
+                $stmt = $conn->query($sql); // Simple, but has several drawbacks
+                $stmt->execute();
+                $row = $stmt->fetch();
+                if ($stmt->rowCount() != 0){
+                    while ($row = $stmt->fetch()){
+                        $cal = new Calendar();
+                        $cal->setTitle($row['title']);
+                        $cal->setDetails($row['details']);
+                        $cal->setStart(new DateTime($row['start']));
+                        $cal->setEnd(new DateTime($row['end']));
+                        $cal->getAllday($row['all_day']);
+                        $cal->setStatus($row['status']);
+                        $cal->setActive($row['active']);
+                        $cal->setUser($user);
+                        $cal->setColor($row['color']);
+                        $cal->setCustomerName($row['Kundenname']);
+                        $cal->setStreet($row['Straße']);
+                        $cal->setStreetNumber($row['HausNr']);
+                        $cal->setPlz($row['PLZ']);
+                        $cal->setOrt($row['Ort']);
+                        $cal->setAnsprechpartner($row['Ansprechpartner']);
+                        $cal->setAnrede($row['anrede']);
+                        $cal->setEmail($row['Emailadresse']);
+                        $cal->setBermerkungen($row['Bemerkungen']);
+                        $cal->setTerminType($row['calTerminTppe']);
+                        $entityManager->persist($cal);
+                        $entityManager->flush();
+                        $count++;
+                    }
                 }
             }
+
         }
         /*
         $conn = \Doctrine\DBAL\DriverManager::getConnection($this->connectionParams);
