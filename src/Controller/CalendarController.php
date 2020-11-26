3af52b5a0c0ed86938c802ca8f3ad63f78a7b2ce
Spongebob <obermann@msw-werbeagentur.de>
2020-11-26 18:45:09 +0100

    sdf

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Calendar;
+use App\Repository\CalendarRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncode;
 
@@ -16,6 +18,7 @@ class CalendarController extends AbstractController
      */
     public function index()
     {
+
         return $this->render('calendar.html.twig', [
 
         ]);
@@ -33,15 +36,15 @@ class CalendarController extends AbstractController
             //echo "**".$event->getStart()->format("Y-m-d H:i:s");
             if($event->getAllday() == 1) {
                 $allday = true;
-                $end = $event->getStart()->format("Y-m-d H:i:s");
+                $end = $event->getStart()->format("Y-m-dTH:i:s");
             } else {
                 $allday = false;
-                $end = $event->getEnd()->format("Y-m-d H:i:s");
+                $end = $event->getEnd()->format("Y-m-dTH:i:s");
             }
             $data[] = array(
                 'id' => $event->getId(),
                 'title'=>$event->getTitle(),
-                'start'=>$event->getStart()->format("Y-m-d H:i:s"),
+                'start'=>$event->getStart()->format("Y-m-dTH:i:s"),
                 'end' => $end,
                 'allDay' => $allday,
                 'details' => $event->getDetails(),
@@ -57,6 +60,49 @@ class CalendarController extends AbstractController
                 'backgroundColor' => $event->getColor(),
                 'calTerminTppe' => $event->getTerminType()
             );
+        }
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @param CalendarRepository $calendarRepository
+     * @param Request $request
+     * @Route ("/calendars/getEvents")
+     */
+    public function getEvents(CalendarRepository $calendarRepository,Request $request)
+    {
+        $user = $this->getUser();
+        $events = $calendarRepository->findBy(array('user' => $user));
+        $data = array();
+        foreach ($events as $event) {
+            //dd($event);
+            //echo "**".$event->start;
+            if ($event->getAllday() == 1) {
+                $allday = true;
+                $end = $event->getStart()->format("Y-m-dTH:i:s");
+            } else {
+                $allday = false;
+                $end = $event->getEnd()->format("Y-m-dTH:i:s");
+            }
+            $data[] = array(
+                'id' => $event->getId(),
+                'title' => $event->getTitle(),
+                'start' => $event->getStart()->format("Y-m-dTH:i:s"),
+                'end' => $end,
+                'allDay' => $allday,
+                'details' => $event->getDetails(),
+                'Kundenname' => $event->getCustomerName(),
+                'Strasse' => $event->getStreet(),
+                'HausNr' => $event->getStreetNumber(),
+                'PLZ' => $event->getPlz(),
+                'Ort' => $event->getOrt(),
+                'Ansprechpartner' => $event->getAnsprechpartner(),
+                'anrede' => $event->getAnrede(),
+                'Emailadresse' => $event->getEmail(),
+                'Bemerkungen' => $event->getBermerkungen(),
+                'backgroundColor' => $event->getColor()
+            );
+
         }
         return new JsonResponse($data);
     }
