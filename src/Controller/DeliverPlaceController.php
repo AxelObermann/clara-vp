d27108b86ca8ce4706ab9772d805842638cc2867
Spongebob <obermann@msw-werbeagentur.de>
2020-11-30 09:27:41 +0100

    sdf

--- src/Controller/DeliverPlaceController.php
+++ src/Controller/DeliverPlaceController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\CustomerRepository;
+use App\Repository\DeliveryPlaceRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DeliverPlaceController extends AbstractController
@@ -15,5 +20,19 @@ class DeliverPlaceController extends AbstractController
         return $this->render('deliver_place/index.html.twig', [
             'controller_name' => 'DeliverPlaceController',
         ]);
+    }
+
+    /**
+     * @param DeliveryPlaceRepository $deliveryPlaceRepository
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @Route ("deliverPlace_move_customer/{id}")
+     */
+    public function moveDPCustomer(CustomerRepository $customerRepository, DeliveryPlaceRepository $deliveryPlaceRepository, Request $request, EntityManagerInterface $entityManager){
+        $params = $request->get('id');
+
+        $params = explode(',', $params);
+        dd($params);
+
     }
 }
