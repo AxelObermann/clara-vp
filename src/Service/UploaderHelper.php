29b1042be9971c6bb60b1ff908eda25706c0af92
Spongebob <obermann@msw-werbeagentur.de>
2020-12-11 17:03:00 +0100

    sdf

--- src/Service/UploaderHelper.php
+++ src/Service/UploaderHelper.php
@@ -36,8 +36,8 @@ class UploaderHelper
         $destination = $this->uploadsPath.$uploadPathFromAjax.'/';
         $newFileName = pathinfo($uploadedFile->getClientOriginalName(),PATHINFO_FILENAME)."-".uniqid().".".$uploadedFile->guessExtension();
         $uploadedFile->move($destination,$newFileName);
-        //dd($uploadPathFromAjax);
-        return true;
+        //dd($uploadPathFromAjax."/".$newFileName);
+        return $newFileName;
     }
 
     public function uploadFacilityFile(UploadedFile $uploadedFile,$uploadPathFromAjax):string{
